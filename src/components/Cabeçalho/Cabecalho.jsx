4b2f78e8ec6e8f0955f4d0689766616fee731e0b
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-11-10 11:08:44 -0300

    Esqueleto do Inicio

--- src/components/Cabeçalho/Cabecalho.jsx
+++ src/components/Cabeçalho/Cabecalho.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logoCabecalho from '../../img/logo.png';
+/* import logoCabecalho from '../../img/logo.png'; */
 import './style.css';
 
 export default class Cabecalho extends Component {
   render() {
     return (
       <div className="header">
-        <img src={ logoCabecalho } className="CompanyLogo" alt="logo-Rodrigo-Sena" />
+        Logo Aqui
+        {/* <img src={ logoCabecalho } className="CompanyLogo" alt="logo-Rodrigo-Sena" /> */}
         <div className="header-right">
           <a>Início</a>
           <a>Sobre mim</a>
--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import './style.css';
 
 export default class Inicio extends Component {
   render() {
     return (
-      <div>Inicio</div>
+      <div className="Inicio">
+        <p>Olá, Bem vindo ao meu Portfólio</p>
+        <h1>Rodrigo Sena Dias</h1>
+        <h2>Desenvolvedor Full Stack</h2>
+        <p>Admirador de tecnologia e estudante da trybe</p>
+      </div>
     );
   }
 }
